2f0a32e9a6921771489404cc81dbed085a42e091
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-05-31 14:36:40 +0700

    membuat if di dalam void insert

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -13,4 +13,14 @@ class Queues {
 
     void insert() {
         int num;
-    
+        cout << "Enter a number:";
+        cin >> num;
+        cout << endl;
+
+        //cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+    }
+}
