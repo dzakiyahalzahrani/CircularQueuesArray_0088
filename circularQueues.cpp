93aea1f5312d9a46508300db4b6c4712653795f6
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-05-31 14:46:40 +0700

    membuat else

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -27,4 +27,10 @@ class Queues {
             FRONT = 0;
             REAR = 0;
         }
-    
+        else {
+            //jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+            REAR = 0;
+            else 
+            REAR = REAR + 1;
+        }
